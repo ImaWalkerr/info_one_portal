b5e63d9dd6a57ebaf0530041b3ced12ad72df4ad
ImaWalkerr <75856607+ImaWalkerr@users.noreply.github.com>
2022-05-30 17:36:23 +0300

    Rebuild weather wrapper
    Added base cities info, done with wrapper functional

--- info_one_portal_main_app/api/weather_wrapper.py
+++ info_one_portal_main_app/api/weather_wrapper.py
@@ -5,19 +5,74 @@ from decouple import config
 
 class WeatherApiWrapper:
     """Weather api"""
-    def get_weather_info_by_city(self, weather_search=''):
+    WEATHER_USER_API_KEY = config('WEATHER_USER_API_KEY')
+    WEATHER_API_URL = config('WEATHER_API_URL')
+    BASE_LOCATION_1 = config('BASE_LOCATION_1')
+    BASE_LOCATION_2 = config('BASE_LOCATION_2')
+    BASE_LOCATION_3 = config('BASE_LOCATION_3')
+    BASE_LOCATION_4 = config('BASE_LOCATION_4')
+    ALL_BASE_LOCATIONS = [BASE_LOCATION_1, BASE_LOCATION_2, BASE_LOCATION_3, BASE_LOCATION_4]
 
-        user_api_key = config('WEATHER_KEY')
-        location = weather_search
+    # weather api request for searched city by user
+    def get_weather_by_city_search(self, weather_search=''):
 
-        api_link_for_city = f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid={user_api_key}'
+        api_link_for_city = f'{self.WEATHER_API_URL}?q={weather_search}&appid={self.WEATHER_USER_API_KEY}'
         api_link_city = requests.get(api_link_for_city)
         api_data_city = api_link_city.json()
 
+        data_for_search_city = []
+
         if api_data_city['cod'] == '404':
-            print('Invalid city: {}, please check your city name and try again'.format(location))
+            raise Exception('Incorrect city name, please check your city name and try again.')
         else:
-            return api_data_city
+            data_search_city = {
+                'city_temp': float('{:.3f}'.format((api_data_city['main'].get('temp')) - 273.15)),
+                'city_pressure': api_data_city['main'].get('pressure'),
+                'city_humidity': api_data_city['main'].get('humidity'),
+                'weather_description': api_data_city['weather'][0].get('description'),
+                'wind_speed': api_data_city['wind'].get('speed'),
+                'date_time': datetime.now().strftime('%d %b %Y | %I:%M:%S: %p')
+            }
+            data_for_search_city.append(data_search_city)
+        return data_for_search_city
+
+    # weather api request for base 4 cities for main_page.html
+    def get_weather_for_bases_cities(self):
+
+        all_weather_data = []
+
+        # add weather information to each city to one variable
+        for location in self.ALL_BASE_LOCATIONS:
+            api_link_for_base_locations = f'{self.WEATHER_API_URL}?q={location}&appid={self.WEATHER_USER_API_KEY}'
+            api_link_base_locations = requests.get(api_link_for_base_locations)
+            api_data_base_locations = api_link_base_locations.json()
+            if api_data_base_locations['cod'] == '404':
+                raise Exception('Incorrect city name, please check your city name and try again.')
+            else:
+                if location == self.ALL_BASE_LOCATIONS[0]:
+                    all_weather_data.append(api_data_base_locations)
+                elif location == self.ALL_BASE_LOCATIONS[1]:
+                    all_weather_data.append(api_data_base_locations)
+                elif location == self.ALL_BASE_LOCATIONS[2]:
+                    all_weather_data.append(api_data_base_locations)
+                elif location == self.ALL_BASE_LOCATIONS[3]:
+                    all_weather_data.append(api_data_base_locations)
+
+        weather_data = []
+
+        # convert data to readable format
+        for item in all_weather_data:
+            weather = {
+                'city_name': item.get('name'),
+                'city_temp': float('{:.3f}'.format((item['main'].get('temp')) - 273.15)),
+                'city_pressure': item['main'].get('pressure'),
+                'city_humidity': item['main'].get('humidity'),
+                'weather_description': item['weather'][0].get('description'),
+                'wind_speed': item['wind'].get('speed'),
+                'date_time': datetime.now().strftime('%d %b %Y | %I:%M:%S: %p')
+            }
+            weather_data.append(weather)
+        return weather_data
 
 
 WEATHER_API_WRAPPER = WeatherApiWrapper()
